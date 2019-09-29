9388f180d8716136097e13209bfb6017d91fbc78
Kevin Hayden <kevin.fhayden@gmail.com>
2019-09-29 22:58:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,45 @@
 class Owner
-  # code goes here
+  
+  @@all = []
+  @@count = 0
+  
+  attr_accessor :cats
+  attr_reader :name, :species, :say_species
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@count += 1
+    @cats = Cat.all
+  end
+  
+  def species
+    @species = "human"
+  end
+  
+  def say_species
+    @say_species = "I am a #{species}."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.reset_all
+    @@all.clear
+    @@count = 0
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  
+  def buy_dog(name)
+  end
+  
 end
